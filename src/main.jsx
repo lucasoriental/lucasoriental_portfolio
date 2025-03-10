38d20a432dd4b3afdd4cee0ef8fa7f20456580a5
lucasoriental <lucasoriental16@gmail.com>
2025-03-10 15:06:55 +0000

    fix 2

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
-
+import ReactGA from "react-ga4";
 import "./i18n";
 
+ReactGA.initialize("G-RHQBPREZL1");
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+})
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
