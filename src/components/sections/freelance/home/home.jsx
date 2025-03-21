52394602ec1770d20e32c653b9c1d2e02dbddf08
lucasoriental <lucasoriental16@gmail.com>
2025-03-21 13:09:22 +0000

    new updates

--- src/components/sections/freelance/home/home.jsx
+++ src/components/sections/freelance/home/home.jsx
@@ -5,12 +5,12 @@ import { Typewriter } from "react-simple-typewriter";
 
 const Home = () => {
   return (
-    <div className="dark: pt-36">
-      <div className="w-[90%] max-w-3xl m-auto items-center flex flex-col lg:flex-row">
-        <div className="flex flex-col items-center bg-cyan-500">
-          <h1 className="text-4xl font-extrabold text text-zinc-900 mb-5 text-center select-none lg:text-start transition-shadow duration-500">
+    <main className="dark: pt-36 pb-36">
+      <div className="w-[90%] max-w-4xl m-auto items-center flex flex-col lg:flex-row lg:flex-wrap">
+        <div className="flex flex-col items-center lg:basis-3/5">
+          <h1 className="text-5xl font-extrabold text-center text-zinc-900 mb-5 select-none lg:text-start">
             Transformo a tua ideia num negócio digital{" "}
-            <span className="text-red-500 font-black transition-shadow duration-500">
+            <span className="text-red-500 font-black inline-block min-w-32 text-nowrap">
               <Typewriter
                 words={["de Sucesso!", "Incrível!", "Perfeito!"]}
                 loop={true}
@@ -22,32 +22,43 @@ const Home = () => {
               />
             </span>
           </h1>
-          <p className="text-lg font-medium text text-zinc-900 mb-7  text-center select-none lg:text-start transition-shadow duration-500">
+          <p className="text-lg font-medium text-zinc-500 mb-7 text-center select-none lg:text-start">
             Não precisas fazer tudo sozinho – juntos, podemos criar a estratégia
             certa para o teu negócio brilhar no digital.
           </p>
-          <div className="bg-green-500 lg:flex lg:justify-center">
-            <button className="flex bg-red-600 pt-3 pb-3 w-64 justify-center border-none rounded-xl mb-5">
-              <p className="text-white text-sm font-light">
+          <div className="lg:flex lg:self-start lg:gap-6 items-center">
+            <button className="flex bg-red-600 pt-3 pb-3 w-64 justify-center border-none rounded-xl mb-5 lg:w-fit lg:pl-4 lg:pr-4">
+              <p className="text-white text-sm font-light self-center lg:text-xs">
                 Solicitar um orçamento{" "}
-                <span className="font-medium">gratuito</span>
+                <span className="font-medium lg:text-xs">grátis</span>
               </p>
             </button>
-            <button className="flex bg-gray-300 pt-3 pb-3 w-64 justify-center rounded-xl mb-5">
-              <p className="text-red-600 font-normal text-sm">
+            <button className="flex bg-gray-300 pt-3 pb-3 w-64 justify-center rounded-xl mb-5 lg:w-44">
+              <p className="text-red-600 font-normal text-sm self-center lg:text-xs">
                 Ver meus Serviços
               </p>
             </button>
           </div>
         </div>
-        <img
-          src={iphoneMockup}
-          alt="Profile"
-          className="h-72 mt-11 bg-green-500"
-          /* id="image-home" */
-        />
+        <div className="flex flex-col items-center lg:basis-2/5 mt-5 lg:mt-0">
+          <img
+            src={iphoneMockup}
+            alt="Profile"
+            className="h-[35em] lg:h-96 mb-5"
+            id="image-home"
+          />
+        </div>
       </div>
-    </div>
+      <div className="w-screen mt-36 bg-red-600 p-1">
+        <p className="text-white w-[95%] max-w-96 border mr-auto ml-auto p-4 mt-4 mb-4 text-center rounded-xl">
+          "Se o teu negócio não está na Internet, então o teu negócio estará
+          fora dos negócios."{" "}
+          <span className="text-zinc-200 text-base font-bold">
+            ~ Bill Gates
+          </span>
+        </p>
+      </div>
+    </main>
   );
 };
 
--- src/components/sections/freelance/services/services.jsx
+++ src/components/sections/freelance/services/services.jsx
@@ -3,190 +3,192 @@ import { Check, Code, Palette, ChartNoAxesCombined } from "lucide-react";
 
 const Services = () => {
   return (
-    <div className="max-w-4xl w-[90%] mr-auto ml-auto pt-24 pb-24 font-[Roboto]">
-      <p className="text-center text-2xl font-black mb-3">
-        Os Serviços que Impulsionam o Teu Negócio no Digital
-      </p>
-      <p className="text-center">
-        Com uma abordagem estratégica e personalizada, ajudo a transformar a
-        presença online da tua marca, desde o desenvolvimento até ao marketing
-        digital.
-      </p>
-      <div className="flex gap-5 flex-wrap">
-        <div className="flex flex-col justify-center items-center w-[90%] max-w-96 mr-auto ml-auto">
-          <div className="mt-5 flex items-center">
-            <Code />
-            <p className="ml-3 text-justify font-bold text-xl">
-              Desenvolvimento/Programação
+    <section className="dark: pb-36 pt-36 bg-red-600">
+      <div className="w-[90%] max-w-4xl m-auto items-center flex flex-col lg:flex-row lg:flex-wrap">
+        <p className="text-4xl font-extrabold text-center text-zinc-900 mb-5 select-none lg:text-start">
+          Os Serviços que Impulsionam o Teu Negócio no Digital
+        </p>
+        <p className="text-lg font-medium text text-zinc-500 mb-7 text-center select-none lg:text-start">
+          Com uma abordagem estratégica e personalizada, ajudo a transformar a
+          presença online da tua marca, desde o desenvolvimento até ao marketing
+          digital.
+        </p>
+        <div className="flex gap-5 flex-wrap">
+          <div className="flex flex-col justify-center items-center w-[90%] max-w-96 mr-auto ml-auto">
+            <div className="mt-5 flex items-center">
+              <Code />
+              <p className="ml-3 text-justify font-bold text-xl">
+                Desenvolvimento/Programação
+              </p>
+            </div>
+            <p className="text-justify font-medium text-md mb-2">
+              Transforme a tua presença online com soluções personalizadas e de
+              alta qualidade para o teu negócio digital.
             </p>
-          </div>
-          <p className="text-justify font-medium text-md mb-2">
-            Transforme a tua presença online com soluções personalizadas e de
-            alta qualidade para o teu negócio digital.
-          </p>
-          <div>
-            <div className="flex items-center">
-              <Check className="w-5 mr-2" />
-              <p className="text-justify text-sm">
-                Criação de Websites Completos
-              </p>
-            </div>
-            <div className="flex items-center">
-              <Check className="w-5 mr-2" />
-              <p className="text-justify text-sm">
-                Criação de Comércios Online (E-commerce)
-              </p>
-            </div>
-            <div className="flex items-center">
-              <Check className="w-5 mr-2" />
-              <p className="text-justify text-sm">Criação de Landing Pages</p>
-            </div>
-            <div className="flex items-center">
-              <Check className="w-5 mr-2" />
-              <p className="text-justify text-sm">
-                Criação de Aplicações Móveis
-              </p>
-            </div>
-            <div className="flex items-center">
-              <Check className="w-5 mr-2" />
-              <p className="text-justify text-sm">
-                Manutenção de Websites já Existentes
-              </p>
-            </div>
-            <div className="flex items-center">
-              <Check className="w-5 mr-2" />
-              <p className="text-justify text-sm">
-                Criação de Portfólios Digitais
-              </p>
-            </div>
-            <div className="flex items-center">
-              <Check className="w-5 mr-2" />
-              <p className="text-justify text-sm">
-                Criação de Páginas de Eventos
-              </p>
+            <div>
+              <div className="flex items-center">
+                <Check className="w-5 mr-2" />
+                <p className="text-justify text-sm">
+                  Criação de Websites Completos
+                </p>
+              </div>
+              <div className="flex items-center">
+                <Check className="w-5 mr-2" />
+                <p className="text-justify text-sm">
+                  Criação de Comércios Online (E-commerce)
+                </p>
+              </div>
+              <div className="flex items-center">
+                <Check className="w-5 mr-2" />
+                <p className="text-justify text-sm">Criação de Landing Pages</p>
+              </div>
+              <div className="flex items-center">
+                <Check className="w-5 mr-2" />
+                <p className="text-justify text-sm">
+                  Criação de Aplicações Móveis
+                </p>
+              </div>
+              <div className="flex items-center">
+                <Check className="w-5 mr-2" />
+                <p className="text-justify text-sm">
+                  Manutenção de Websites já Existentes
+                </p>
+              </div>
+              <div className="flex items-center">
+                <Check className="w-5 mr-2" />
+                <p className="text-justify text-sm">
+                  Criação de Portfólios Digitais
+                </p>
+              </div>
+              <div className="flex items-center">
+                <Check className="w-5 mr-2" />
+                <p className="text-justify text-sm">
+                  Criação de Páginas de Eventos
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="flex flex-col justify-center items-center w-[90%] max-w-96 mr-auto ml-auto">
-          <div className="mt-5 flex items-center">
-            <Palette />
-            <p className="ml-3 text-justify font-bold text-xl">Design</p>
-          </div>
-          <p className="text-justify font-medium text-md mb-2">
-            Dê vida à identidade visual da tua marca com designs criativos e que
-            encantam os teus clientes.
-          </p>
-          <div>
-            <div className="flex items-center">
-              <Check className="w-5 mr-2" />
-              <p className="text-justify text-sm">
-                Criação de Interface para Websites
-              </p>
+          <div className="flex flex-col justify-center items-center w-[90%] max-w-96 mr-auto ml-auto">
+            <div className="mt-5 flex items-center">
+              <Palette />
+              <p className="ml-3 text-justify font-bold text-xl">Design</p>
             </div>
-            <div className="flex items-center">
-              <Check className="w-5 mr-2" />
-              <p className="text-justify text-sm">
-                Criação de Interface para Aplicações Móveis
-              </p>
-            </div>
-            <div className="flex items-center">
-              <Check className="w-5 mr-2" />
-              <p className="text-justify text-sm">
-                Criação de Páginas Web Específicas
-              </p>
-            </div>
-            <div className="flex items-center">
-              <Check className="w-5 mr-2" />
-              <p className="text-justify text-sm">
-                Criação de Posts para Redes Sociais
-              </p>
-            </div>
-            <div className="flex items-center">
-              <Check className="w-5 mr-2" />
-              <p className="text-justify text-sm">
-                Criação de Banners e Thumbnails
-              </p>
-            </div>
-            <div className="flex items-center">
-              <Check className="w-5 mr-2" />
-              <p className="text-justify text-sm">
-                Criação de Flyers e Folders
-              </p>
-            </div>
-            <div className="flex items-center">
-              <Check className="w-5 mr-2" />
-              <p className="text-justify text-sm">
-                Criação de Cartões Empresariais
-              </p>
-            </div>
-            <div className="flex items-center">
-              <Check className="w-5 mr-2" />
-              <p className="text-justify text-sm">
-                Criação de Apresentações Digitais (Slides)
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col justify-center items-center w-[90%] max-w-96 mr-auto ml-auto">
-          <div className="mt-5 flex items-center">
-            <ChartNoAxesCombined />
-            <p className="ml-3 text-justify font-bold text-xl">
-              Marketing Digital
+            <p className="text-justify font-medium text-md mb-2">
+              Dê vida à identidade visual da tua marca com designs criativos e
+              que encantam os teus clientes.
             </p>
+            <div>
+              <div className="flex items-center">
+                <Check className="w-5 mr-2" />
+                <p className="text-justify text-sm">
+                  Criação de Interface para Websites
+                </p>
+              </div>
+              <div className="flex items-center">
+                <Check className="w-5 mr-2" />
+                <p className="text-justify text-sm">
+                  Criação de Interface para Aplicações Móveis
+                </p>
+              </div>
+              <div className="flex items-center">
+                <Check className="w-5 mr-2" />
+                <p className="text-justify text-sm">
+                  Criação de Páginas Web Específicas
+                </p>
+              </div>
+              <div className="flex items-center">
+                <Check className="w-5 mr-2" />
+                <p className="text-justify text-sm">
+                  Criação de Posts para Redes Sociais
+                </p>
+              </div>
+              <div className="flex items-center">
+                <Check className="w-5 mr-2" />
+                <p className="text-justify text-sm">
+                  Criação de Banners e Thumbnails
+                </p>
+              </div>
+              <div className="flex items-center">
+                <Check className="w-5 mr-2" />
+                <p className="text-justify text-sm">
+                  Criação de Flyers e Folders
+                </p>
+              </div>
+              <div className="flex items-center">
+                <Check className="w-5 mr-2" />
+                <p className="text-justify text-sm">
+                  Criação de Cartões Empresariais
+                </p>
+              </div>
+              <div className="flex items-center">
+                <Check className="w-5 mr-2" />
+                <p className="text-justify text-sm">
+                  Criação de Apresentações Digitais (Slides)
+                </p>
+              </div>
+            </div>
           </div>
-          <p className="text-justify font-medium text-md mb-2">
-            Amplie o alcance da tua marca e maximize lucros com estratégias
-            eficazes de marketing digital.
-          </p>
-          <div>
-            <div className="flex items-center">
-              <Check className="w-5 mr-2" />
-              <p className="text-justify text-sm">Gestão de Mídias Sociais</p>
-            </div>
-            <div className="flex items-center">
-              <Check className="w-5 mr-2" />
-              <p className="text-justify text-sm">
-                Criação de Loja no Instagram
+          <div className="flex flex-col justify-center items-center w-[90%] max-w-96 mr-auto ml-auto">
+            <div className="mt-5 flex items-center">
+              <ChartNoAxesCombined />
+              <p className="ml-3 text-justify font-bold text-xl">
+                Marketing Digital
               </p>
             </div>
-            <div className="flex items-center">
-              <Check className="w-5 mr-2" />
-              <p className="text-justify text-sm">Gestão de tráfego Pago</p>
-            </div>
-            <div className="flex items-center">
-              <Check className="w-5 mr-2" />
-              <p className="text-justify text-sm">
-                Criação de Páginas e Grupos no Facebook
-              </p>
-            </div>
-            <div className="flex items-center">
-              <Check className="w-5 mr-2" />
-              <p className="text-justify text-sm">
-                Análise de Resultados de Marketing
-              </p>
-            </div>
-            <div className="flex items-center">
-              <Check className="w-5 mr-2" />
-              <p className="text-justify text-sm">
-                Criação e Edição de Reels e TikToks
-              </p>
-            </div>
-            <div className="flex items-center">
-              <Check className="w-5 mr-2" />
-              <p className="text-justify text-sm">
-                Criação de Estratégia para maximizar lucro
-              </p>
-            </div>
-            <div className="flex items-center">
-              <Check className="w-5 mr-2" />
-              <p className="text-justify text-sm">
-                Criação de Campanhas Online
-              </p>
+            <p className="text-justify font-medium text-md mb-2">
+              Amplie o alcance da tua marca e maximize lucros com estratégias
+              eficazes de marketing digital.
+            </p>
+            <div>
+              <div className="flex items-center">
+                <Check className="w-5 mr-2" />
+                <p className="text-justify text-sm">Gestão de Mídias Sociais</p>
+              </div>
+              <div className="flex items-center">
+                <Check className="w-5 mr-2" />
+                <p className="text-justify text-sm">
+                  Criação de Loja no Instagram
+                </p>
+              </div>
+              <div className="flex items-center">
+                <Check className="w-5 mr-2" />
+                <p className="text-justify text-sm">Gestão de tráfego Pago</p>
+              </div>
+              <div className="flex items-center">
+                <Check className="w-5 mr-2" />
+                <p className="text-justify text-sm">
+                  Criação de Páginas e Grupos no Facebook
+                </p>
+              </div>
+              <div className="flex items-center">
+                <Check className="w-5 mr-2" />
+                <p className="text-justify text-sm">
+                  Análise de Resultados de Marketing
+                </p>
+              </div>
+              <div className="flex items-center">
+                <Check className="w-5 mr-2" />
+                <p className="text-justify text-sm">
+                  Criação e Edição de Reels e TikToks
+                </p>
+              </div>
+              <div className="flex items-center">
+                <Check className="w-5 mr-2" />
+                <p className="text-justify text-sm">
+                  Criação de Estratégia para maximizar lucro
+                </p>
+              </div>
+              <div className="flex items-center">
+                <Check className="w-5 mr-2" />
+                <p className="text-justify text-sm">
+                  Criação de Campanhas Online
+                </p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
