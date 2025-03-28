cea9727f6d28b6a54845be852ad82c2e436918cb
lucasoriental <lucasoriental16@gmail.com>
2025-03-28 07:56:58 +0000

    new changes in the Testimonials Page

--- src/pages/Freelance/sections/testimonials/testimonials.jsx
+++ src/pages/Freelance/sections/testimonials/testimonials.jsx
@@ -5,7 +5,7 @@ import { useTranslation } from "react-i18next";
 const Testimonials = () => {
 const { t } = useTranslation("freelance");
   return (
-    <section className="dark: pb-36 pt-36">
+    <section id="container_testimonials" className="dark: pb-36 pt-36">
       <div className="w-screen max-w-[90%] mr-auto ml-auto">
         <div className="mx-auto w-[90%] max-w-4xl mb-12">
           <p className="text-4xl font-extrabold text-center text-zinc-900 mb-5 select-none lg:text-start lg:w-[20em]">
