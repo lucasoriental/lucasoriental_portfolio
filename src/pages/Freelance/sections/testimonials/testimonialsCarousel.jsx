709a682b2d590a25de31d83ac4e5f00106278dd4
lucasoriental <lucasoriental16@gmail.com>
2025-03-28 14:52:55 +0000

    small fix

--- src/pages/Freelance/sections/testimonials/testimonialsCarousel.jsx
+++ src/pages/Freelance/sections/testimonials/testimonialsCarousel.jsx
@@ -17,7 +17,7 @@ const testimonials = [
     img: caioVaz,
     quote:
       "Lucas é um profissional dedicado, preciso e inovador. Com um olhar estratégico e atenção aos detalhes, entrega soluções funcionais e alinhadas às necessidades do projeto. Além da competência técnica, tem grande capacidade de adaptação e resolução de problemas, sendo um parceiro confiável para qualquer equipe. Seu compromisso com a excelência garante resultados eficientes.",
-    name: "Caio Marcelo vaz",
+    name: "Caio Marcelo Vaz",
     position: "Desenvolvedor Front-end",
   },
 ];
