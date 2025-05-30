25580abfd7df1ec80d38434079f330213fdb4238
lucasoriental <lucasoriental16@gmail.com>
2025-05-30 21:24:42 +0100

    Setting up the eslint properly, fixing some dependencies and applying eslint into vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  css: {preprocessorOptions: {
-    scss: {
-      api: "modern-compiler"
-    }
-  }},
-  plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
+  plugins: [react(), eslint()],
+});
